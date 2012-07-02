7a35b236bf92ec048eb6962417d9d670c493b892
chapmanbe@gmail.com <chapmanbe@gmail.com@840a737e-d840-11dd-93da-dbb4cac96d7c>
2012-07-02 10:02:58 +0000

    added test for sentence splitting without final sentence termination

--- trunk/src/pyConTextNLP/test1.py
+++ trunk/src/pyConTextNLP/test1.py
@@ -1,13 +1,14 @@
 import unittest
 import itemData
 import pyConTextGraph as pyConText
-
+import helpers
 class imageTools_test(unittest.TestCase):
     def setUp(self):
         # create a sample image in memory
         self.context = pyConText.pyConText()
         self.su1 = u'kanso <Diagnosis>**diabetes**</Diagnosis> utesl\xf6t eller diabetes men inte s\xe4kert. Vi siktar p\xe5 en r\xf6ntgenkontroll. kan det vara nej panik\xe5ngesten\n?'
         self.su2 =  u'IMPRESSION: 1. LIMITED STUDY DEMONSTRATING NO GROSS EVIDENCE OF SIGNIFICANT PULMONARY EMBOLISM.'
+        self.su3 = u'This is a sentence that does not end with a number. But this sentence ends with 1.'
     def tearDown(self):
         self.context = 0
         self.su1 = 0
@@ -24,6 +25,11 @@ class imageTools_test(unittest.TestCase):
         self.context.setRawText(self.su2)
         self.context.cleanText()
         assert self.context.getText().rfind(u'.') == -1
+    def text_sentenceSplitter1(self):
+        """test whether we properly capture text that terminates without a recognized sentence termination"""
+        sentences = helpers.sentenceSplitter(self.su3)
+        assert len(sentences) == 2
+
     # add function to test DocumentGraph generation  
 def run():
     pass
