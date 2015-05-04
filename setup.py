488f6cfab2ae48e421dc0c8792bcc396419390d7
Brian Chapman <brian.chapman@utah.edu>
2015-05-04 15:24:48 -0600

    split domain_kb_test into two separate files

--- setup.py
+++ setup.py
@@ -39,16 +39,16 @@ setup(
         'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Students',
-        'Topic :: Software Development :: Build Tools',
+        #'Intended Audience :: Students',
+        #'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache2',
+        #'License :: OSI Approved :: Apache2',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
+        #'Programming Language :: Python :: 2',
+        #'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
     ],
 
