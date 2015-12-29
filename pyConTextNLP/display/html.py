b926b0b99ad5a236efa2a17c775e7c81eea8945a
Brian Chapman <brian.chapman@utah.edu>
2015-12-29 16:07:38 -0700

    unknown changes

--- pyConTextNLP/display/html.py
+++ pyConTextNLP/display/html.py
@@ -7,12 +7,12 @@ def __sort_by_span(nodes):
     n.sort(key=lambda x: x.getSpan())
     return n
 def __insert_color(txt,s,c):
-    """insert HTML span style into txt. The span will change the color of the 
+    """insert HTML span style into txt. The span will change the color of the
     text located between s[0] and s[1]:
     txt: txt to be modified
     s: span of where to insert tag
     c: color to set the span to"""
-    return txt[:s[0]]+'<span style="color: %s;">'%c+\
+    return txt[:s[0]]+'<span style="color: {0};">'.format(c)+\
            txt[s[0]:s[1]]+'</span>'+txt[s[1]:]
 
 def mark_text(txt,nodes,colors = {"name":"red","pet":"blue"},default_color="blue"):
@@ -27,16 +27,14 @@ def mark_text(txt,nodes,colors = {"name":"red","pet":"blue"},default_color="blue
                          colors=colors)
 
 def mark_document_with_html(doc,colors = {"name":"red","pet":"blue"}, default_color="blue"):
-    """takes a ConTextDocument object and returns an HTML paragraph with marked phrases in the 
+    """takes a ConTextDocument object and returns an HTML paragraph with marked phrases in the
     object highlighted with the colors coded in colors
-    
+
     doc: ConTextDocument
     colors: dictionary keyed by ConText category with values valid HTML colors
-    
+
     """
-    return """<p> %s </p>"""%" ".join([mark_text(m.graph['__txt'],
+    return """<p> {0} </p>""".format(" ".join([mark_text(m.graph['__txt'],
                                                  __sort_by_span(m.nodes()),
                                                  colors=colors,
-                                                 default_color=default_color) for m in get_document_markups(doc)])
-
-
+                                                 default_color=default_color) for m in get_document_markups(doc)]))
